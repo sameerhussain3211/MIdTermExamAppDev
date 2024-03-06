d9163a0276c588df0d53e338f5030966097434b0
sameerhussain3211 <sameerhussain44332211@gmail.com>
2024-03-06 16:57:22 +0500

    commit

--- mid_exam_app/lib/homePage.dart
+++ mid_exam_app/lib/homePage.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:mid_exam_app/launch_model.dart';
+import 'package:readmore/readmore.dart';
 
 class homePage extends StatefulWidget {
   @override
@@ -10,12 +12,157 @@ class homePage extends StatefulWidget {
   }
 }
 
+bool isReadMore = false;
+
 class _homePageState extends State<homePage> {
+  String text = "sameer is nice boy and he like playing footvballl";
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello"),
+          backgroundColor: Color.fromARGB(255, 40, 141, 100),
+          title: const Text(
+            "Space Missions",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          )),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            List products = snapshot.data!;
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  Launch item = products[index];
+                  return Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Container(
+                      child: Card(
+                        elevation: 5,
+                        shadowColor: Colors.green,
+                        child: ListTile(
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                // height: 50,
+                                child: Text(
+                                  "${item.missionName}",
+                                  textAlign: TextAlign.start,
+                                ),
+                              ),
+                              Text(
+                                "${item.description}",
+                                textAlign: TextAlign.center,
+                                maxLines: isReadMore ? 10 : 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              SizedBox(height: 20),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: 250,
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isReadMore = !isReadMore;
+                                      });
+                                    },
+                                    child: Text(
+                                        isReadMore ? "Read less" : "Read more"),
+                                  ),
+                                ],
+                              ),
+                              Chip(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                avatar: CircleAvatar(
+                                  backgroundColor: Colors.grey.shade800,
+                                  child: null,
+                                ),
+                                label: Text("${item.payloadIds![0]}"),
+                              ),
+                              Chip(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                avatar: CircleAvatar(
+                                  backgroundColor: Colors.grey.shade800,
+                                  child: null,
+                                ),
+                                label: Text("${item.payloadIds![1]}"),
+                              ),
+                              Chip(
+                                shape: const RoundedRectangleBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(20))),
+                                avatar: CircleAvatar(
+                                  backgroundColor: Colors.grey.shade800,
+                                  child: null,
+                                ),
+                                label: Text("${item.payloadIds![1]}"),
+                              ),
+                              // grid.builder(
+                              //     itemCount: item.payloadIds!.length,
+                              //     itemBuilder: (context, index) {
+                              //       return Container(
+                              //         child: Text("hello"),
+                              //       );
+                              //     })
+                              // GridView.builder(
+                              //     gridDelegate:
+                              //         const SliverGridDelegateWithMaxCrossAxisExtent(
+                              //             maxCrossAxisExtent: 200,
+                              //             childAspectRatio: 3 / 2,
+                              //             crossAxisSpacing: 20,
+                              //             mainAxisSpacing: 20),
+                              //     itemCount: item.payloadIds!.length,
+                              //     itemBuilder: (BuildContext ctx, index) {
+                              //       return Container(
+                              //         alignment: Alignment.center,
+                              //         decoration: BoxDecoration(
+                              //             color: Colors.amber,
+                              //             borderRadius:
+                              //                 BorderRadius.circular(15)),
+                              //         child: Text("hello"),
+                              //       );
+                              //     }),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Text("no data availables");
+            }
+          }
+        },
       ),
     );
+  }
+
+  Future<List<Launch>> getData() async {
+    final response =
+        await http.get(Uri.parse('https://api.spacexdata.com/v3/missions'));
+
+    if (response.statusCode == 200) {
+      final jsonResponse = jsonDecode(response.body.toString()) as List;
+      List<Launch> products =
+          jsonResponse.map((e) => Launch.fromJson(e)).toList();
+
+      return products;
+    } else {
+      throw Exception("Failed to get data");
+    }
   }
 }
